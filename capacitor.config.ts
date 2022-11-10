44036c9c12ade94eb1eba1213e3573d6a7cb5097
my life kinda sucks <nguyendtgiang@gmail.com>
2022-11-10 14:25:15 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.cw',
+  appName: 'ionic-cw',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
